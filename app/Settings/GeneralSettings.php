7f80cf50096c8f19252ab2698cf703681fe85d39
Tawanda Joseph Mutsena <ndeip.mhondoro@gmail.com>
2025-03-11 16:24:07 +0200

    google analytics working start on gen settings

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -6,9 +6,10 @@ use Spatie\LaravelSettings\Settings;
 
 class GeneralSettings extends Settings
 {
-    // Define properties to store logo and favicon URLs
-    public string $logo;
-    public string $favicon;
+    public ?string $site_logo = null;
+    public ?string $site_favicon = null;
+    public ?string $site_name = null;
+    public ?string $site_description = null;
 
     // Group the settings to make it manageable and distinguishable
     public static function group(): string
