eddd9b1000285fcc9dd394ca976465e79b519778
Tawanda Joseph Mutsena <afroprenuer@example.com>
2025-03-21 00:39:28 +0200

    Fabricator image content card

--- app/Filament/Fabricator/PageBlocks/ImageContentCard01.php
+++ app/Filament/Fabricator/PageBlocks/ImageContentCard01.php
@@ -4,6 +4,9 @@ namespace App\Filament\Fabricator\PageBlocks;
 
 use Filament\Forms\Components\Builder\Block;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Awcodes\Curator\Components\Forms\CuratorPicker;
 
 class ImageContentCard01 extends PageBlock
 {
@@ -11,7 +14,22 @@ class ImageContentCard01 extends PageBlock
     {
         return Block::make('image-content-card01')
             ->schema([
-                
+                CuratorPicker::make('image')
+                ->label('image')
+                ->buttonLabel('Choose image')
+                ->required(),
+            TextInput::make('image_alt')
+                ->label('Alt Text')
+                ->required(),
+
+                TextInput::make('heading')
+                ->label('Title')
+                ->required(),
+                TextInput::make('subheading')
+                ->label('Subtitle')
+                ->required(),
+                Textarea::make('button'),
+                Textarea::make('button_link'),
             ]);
     }
 
--- resources/views/components/filament-fabricator/page-blocks/image-content-card01.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/image-content-card01.blade.php
@@ -2,24 +2,30 @@
 
 
 
-<!--image-->
+@props([
+  'image',
+  'image_alt',
+    'heading',
+    'subheading',
+    'button',
+    'button_link',
+])
 
 
 <section class="bg-white px-4 py-8 antialiased dark:bg-gray-900 md:py-16">
     <div class="mx-auto grid max-w-screen-xl rounded-lg bg-gray-50 p-4 dark:bg-gray-800 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16">
       <div class="lg:col-span-5 lg:mt-0">
-        <a href="#">
-          <img class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components.svg" alt="peripherals" />
-          <img class="mb-4 hidden dark:block md:h-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components-dark.svg" alt="peripherals" />
+       <a href="#">
+          <img class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full" src="{{ $image }}" alt="{{ $image_alt }}" />
+          <img class="mb-4 hidden dark:block md:h-full" src="{{ $image }}" alt="{{ $image_alt }}" />
         </a>
       </div>
       <div class="me-auto place-self-center lg:col-span-7">
-        <h1 class="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">
-          Save $500 today on your purchase <br />
-          of a new iMac computer.
+        <h1 class="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">{{ $heading }}
+        
         </h1>
-        <p class="mb-6 text-gray-500 dark:text-gray-400">Reserve your new Apple iMac 27” today and enjoy exclusive savings with qualified activation. Pre-order now to secure your discount.</p>
-        <a href="#" class="inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"> Pre-order now </a>
+        <p class="mb-6 text-gray-500 dark:text-gray-400">{{ $subheading }}</p>
+        <a href="{{ $button_link }}" class="inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">{{ $button }} </a>
       </div>
     </div>
   </section>
