15e1d2f686973c15deb1ea80b43d767f4f3f70a6
Tawanda Joseph Mutsena <afroprenuer@example.com>
2025-03-21 03:00:36 +0200

    filament info cards 01

--- app/Filament/Fabricator/PageBlocks/InfoCards01.php
+++ app/Filament/Fabricator/PageBlocks/InfoCards01.php
@@ -4,6 +4,8 @@ namespace App\Filament\Fabricator\PageBlocks;
 
 use Filament\Forms\Components\Builder\Block;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 
 class InfoCards01 extends PageBlock
 {
@@ -11,7 +13,42 @@ class InfoCards01 extends PageBlock
     {
         return Block::make('info-cards01')
             ->schema([
-                //
+                TextInput::make('category1')
+                ->label('category')
+                ->required(),
+                TextInput::make('heading1')
+                ->label('Main Title')
+                ->required(),
+                TextInput::make('subheading1')
+                ->label('Subtitle')
+                ->required(),
+                Textarea::make('button1'),
+                TextInput::make('button1Link'),
+
+                TextInput::make('category2')
+                ->label('category')
+                ->required(),
+                TextInput::make('heading2')
+                ->label('2nd section Main Title')
+                ->required(),
+                TextInput::make('subheading2')
+                ->label('Subtitle')
+                ->required(),
+                Textarea::make('button2'),
+                TextInput::make('button2_link'),
+
+                TextInput::make('category3')
+                ->label('category')
+                ->required(),
+                TextInput::make('heading3')
+                ->label('3rd section Main Title')
+                ->required(),
+                TextInput::make('subheading3')
+                ->label('Subtitle')
+                ->required(),
+                Textarea::make('button3'),
+                TextInput::make('button3_link'),
+               
             ]);
     }
 
