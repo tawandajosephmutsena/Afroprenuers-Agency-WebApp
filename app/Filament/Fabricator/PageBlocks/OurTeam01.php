df14e34db4717cbcb4da53ec75cc9513bc95201f
Tawanda Joseph Mutsena <afroprenuer@example.com>
2025-03-22 05:05:24 +0200

    Our-Team01

--- app/Filament/Fabricator/PageBlocks/OurTeam01.php
+++ app/Filament/Fabricator/PageBlocks/OurTeam01.php
@@ -3,6 +3,10 @@
 namespace App\Filament\Fabricator\PageBlocks;
 
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class OurTeam01 extends PageBlock
@@ -11,7 +15,46 @@ class OurTeam01 extends PageBlock
     {
         return Block::make('our-team01')
             ->schema([
-                //
+                TextInput::make('heading')
+                    ->label('Section Heading')
+                    ->default('Our Team')
+                    ->required(),
+                RichEditor::make('description')
+                    ->label('Section Description')
+                    ->default('Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind'),
+                Repeater::make('team_members')
+                    ->schema([
+                        FileUpload::make('image')
+                            ->label('Member Photo')
+                            ->image()
+                            ->directory('team-members')
+                            ->required(),
+                        TextInput::make('name')
+                            ->label('Full Name')
+                            ->required(),
+                        TextInput::make('position')
+                            ->label('Job Position')
+                            ->required(),
+                        RichEditor::make('description')
+                            ->label('Member Description')
+                            ->required(),
+                        TextInput::make('facebook_url')
+                            ->label('Facebook URL')
+                            ->url(),
+                        TextInput::make('twitter_url')
+                            ->label('Twitter URL')
+                            ->url(),
+                        TextInput::make('github_url')
+                            ->label('GitHub URL')
+                            ->url(),
+                        TextInput::make('dribbble_url')
+                            ->label('Dribbble URL')
+                            ->url(),
+                    ])
+                    ->defaultItems(4)
+                    ->collapsible()                  
+                    ->label('Team Members')
+                    ->required(),
             ]);
     }
 
